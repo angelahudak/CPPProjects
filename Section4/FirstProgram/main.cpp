6a48f7bcf79ca816683def8a7d00623d190fbdba
Angela Hudak <43056353+angelahudak@users.noreply.github.com>
2020-07-15 14:42:16 -0400

    Add files via upload

--- Section4/FirstProgram/main.cpp
+++ Section4/FirstProgram/main.cpp
@@ -3,9 +3,8 @@
 int main() {
     int favorite_number;
     
-    std::cout << "Enter your favorite number between 1 and 100:";
+    std::cout << "Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
-    
     std::cout << "Amazing!! Tha's my favorite number too!" << std::endl;
     
     return 0;
