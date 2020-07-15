3e888ac812d9f268f9234d4aa1491c510f0afd8d
Angela Hudak <43056353+angelahudak@users.noreply.github.com>
2020-07-15 15:40:02 -0400

    Update main.cpp

--- Section4/FirstProgram/main.cpp
+++ Section4/FirstProgram/main.cpp
@@ -5,7 +5,7 @@ int main() {
     
     std::cout << "Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
-    std::cout << "Amazing!! Tha's my favorite number too!" << std::endl;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
     
     return 0;
 }
