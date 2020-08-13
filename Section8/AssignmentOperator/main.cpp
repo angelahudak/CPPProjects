44c10e6f7f2ad33d67c9ba461d86bf29161ff843
Angela Hudak <ach2414@g.rit.edu>
2020-08-13 16:56:41 -0400

    Assignment Operator

---
+++ Section8/AssignmentOperator/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    // const int num1 {10}; //This is a read-only variable - cannot be changed
+    int num1 {10}; // initialization not assignment
+    int num2 {20};
+    
+    num1 = 100; // Change the 10 to 100
+    // num1 = num2; // Change the 10 to the value of num2 which is 20
+    // num1 = num2 = 1000; // Change the value of both num1 and num2 to 1000
+    // num1 = "Angela"; // Won't work with string because num1 was declared as an integer
+    // 100 = num1; // 100 is iliteral - no L value - it doesn't have a location in memory
+    
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    
+    cout << endl;
+    return 0;
+}
