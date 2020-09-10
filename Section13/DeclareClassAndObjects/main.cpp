2d636cbf4f1988b6f49e1fd0d0ce5460496f7de3
Angela Hudak <ach2414@g.rit.edu>
2020-09-10 12:49:21 -0400

    Declare Class and Objects

---
+++ Section13/DeclareClassAndObjects/main.cpp
@@ -0,0 +1,55 @@
+// Section 13
+// Declare CLasses and Objects
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Player {
+    
+    // attributes (data)
+    string name {"Player"};
+    int health{100};
+    int xp{3};
+    
+    // methods (operations or functions)
+    void talk(string);
+    bool is_dead();
+    
+};
+
+class Account {
+    // attributes
+    string name {"Account"};
+    double balance{0.0};
+    
+    // methods
+    bool deposit(double);
+    bool withdraw(double);
+    
+    
+    
+};
+
+int main() {
+    Account angela_account;
+    Account jim_account;
+    
+    Player angela;
+    Player hero;
+    
+    Player players[] {angela, hero};
+    
+    // collectoin of player objects 
+    vector<Player> player_vec{angela};
+    player_vec.push_back(hero);
+    
+    Player *enemy {nullptr};
+    enemy = new Player;
+    
+    delete enemy;
+    
+    return 0;
+}
